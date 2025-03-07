0c61008df8a5d7d5e6f5d1e631bc2d94f3b15a96
Shane <palomoshanecraven@gmail.com>
2025-03-07 18:26:59 +0800

    minor addition

--- program10.py
+++ program10.py
@@ -3,22 +3,26 @@
 nums = []
 
 while True:
-    num1 = int(input("Enter 1st number: "))
-    num2 = int(input("Enter 2nd number: "))
+    try:
+        num1 = int(input("Enter 1st number: "))
+        num2 = int(input("Enter 2nd number: "))
 
-    if abs(num1 - num2) > 1:
-        if num1 < num2:
-            for i in range(num1 + 1, num2):
-                if i not in nums:
-                    nums.append(i)
+        if abs(num1 - num2) > 1:
+            if num1 < num2:
+                for i in range(num1 + 1, num2):
+                    if i not in nums:
+                        nums.append(i)
+            else:
+                for i in range(num2 + 1, num1):
+                    if i not in nums:
+                        nums.append(i)
+        
+            print(f"The number between {num1} and {num2}: {nums}")
+            break
+        
         else:
-            for i in range(num2 + 1, num1):
-                if i not in nums:
-                    nums.append(i)
-       
-        print(f"The number between {num1} and {num2}: {nums}")
-        break
-    
-    else:
-        print(f"There is no number between {num1} and {num2}.")
-        break
+            print(f"There is no number between {num1} and {num2}.")
+            break
+
+    except ValueError:
+        print("Invalid input.")
